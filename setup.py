e8037da0bead34b95422edca92d647d160b3fc43
spkosana <spkosana.82@gmail.com>
2022-05-05 09:02:56 -0400

    removing package name change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("small.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='etl-demo',
+    name='code',
     version='0.0.3',
     author='Surya Kosana',
     author_email='spkosana.82@gmail.com',
@@ -14,6 +14,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/spkosana/etl-demo.git',
     license='SKosana',
-    packages=['etl-demo'],
+    packages=['code'],
     install_requires=['pandas'],
 )
